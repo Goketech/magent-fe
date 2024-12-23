f8d056c46c28f200f237412a4bd71c5b8351d1c1
Modupe Akanni <modupe775@gmail.com>
2024-12-23 15:35:26 +0100

    style: Update OfferCard component to include rounded corners in the layout

--- src/components/layouts/OfferCard.tsx
+++ src/components/layouts/OfferCard.tsx
@@ -7,7 +7,7 @@ interface OfferCardProps {
 
 export function OfferCard({ label, title, text, color }: OfferCardProps) {
   return (
-    <div className="bg-[#242424] px-[20px] py-[28px] border border-[#2E2E2E] text-white">
+    <div className="bg-[#242424] px-[20px] py-[28px] border border-[#2E2E2E] text-white rounded-[6px]">
       <span className={`font-[500] text-black text-[13px] leading-[19.5px] rounded-[32px] px-[12px] py-[2px] ${color}`}>{label}</span>
       <h3 className="mt-[28px] mb-[12px] font-[600] text-[24px] leading-[36px]">{title}</h3>
       <p className="text-[14px] font-[400] leading-[21px]">{text}</p>
