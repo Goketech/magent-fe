8e35f58f9bfa7d1c02cba8b4e7b117229078ccd6
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-05-08 10:38:48 +0100

    fix: make adjust to the mobile UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,6 +59,9 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      screens: {
+        'xs': {'max': '400px'},
+      }
     },
   },
   plugins: [require("tailwindcss-animate")],
