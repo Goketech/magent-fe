e64ffdf7ebcfaf509f94c2dab89395b0f1d5d939
Aborisade <bobleeswaggs123@gmail.com>
2025-05-09 11:47:02 +0100

    kpis fix

--- src/components/layouts/CampaignFormBasic.tsx
+++ src/components/layouts/CampaignFormBasic.tsx
@@ -74,7 +74,7 @@ const CampaignFormBasic: React.FC<CampaignFormBasicProps> = ({ formik }) => {
                 formik.setFieldValue("valuePerUser", getAutoValuePerUser(selectedGoal) || "");
               }}
             onBlur={formik.handleBlur}
-            className="w-full p-2 border border-[#D7D7D7] rounded text-[#999999]"
+            className="w-full p-2 border  border-[#D7D7D7] rounded text-[#999999]"
           >
             <option value="">Select goal</option>
             <option value="Engagement">Engagement</option>
@@ -247,7 +247,7 @@ const CampaignFormBasic: React.FC<CampaignFormBasicProps> = ({ formik }) => {
               name="valuePerUser"
               value={formik.values.valuePerUser}
               readOnly
-              className="w-full p-2 border border-[#D7D7D7] rounded text-[#999999] bg-gray-100"
+              className="w-full p-2 border hover:cursor-none focus:cursor-none outline-none border-[#D7D7D7] rounded text-[#999999] bg-gray-100"
             />
           </div>
           {formik.touched.valuePerUser && formik.errors.valuePerUser && (
