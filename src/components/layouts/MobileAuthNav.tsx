d116c5937fdd0daa3af11a76fc5e2e1b2b3ebf62
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-06-10 23:07:32 +0100

    fix: show auth button on mobile screen

--- src/components/layouts/MobileAuthNav.tsx
+++ src/components/layouts/MobileAuthNav.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { ChevronRight } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function MobileAuthNav() {
   const [scrolled, setScrolled] = useState(false);
@@ -17,12 +20,19 @@ function MobileAuthNav() {
     };
   });
 
+  const pathname = usePathname();
+  const isSignUpPage = pathname === "/signup";
+  const isSignInPage = pathname === "/login";
+  const linkHref = isSignUpPage ? "/login" : "/signup";
+  const linkText = isSignUpPage ? "Sign in" : "Sign up";
+
   return (
     <div
       className={`flex w-full justify-between items-center z-10 p-4 fixed top-0 left-0 ${
         scrolled ? "bg-white shadow" : "bg-white"
       }`}
     >
+      <Link href="/">
       <Image
         src="/authLogo.svg"
         alt="Logo"
@@ -30,9 +40,12 @@ function MobileAuthNav() {
         height={70}
         className="cursor-pointer"
       />
+      </Link>
       <div className="flex gap-1 items-center justify-center bg-[#F6F6F6] rounded-[32px] px-4 py-3 text-[#330065] text-sm">
-        <span>Sign In</span>
+        <Link href={linkHref} className="flex items-center gap-1">
+        <span>{linkText}</span>
         <ChevronRight className="h-4 w-4" />
+        </Link>
       </div>
     </div>
   );
