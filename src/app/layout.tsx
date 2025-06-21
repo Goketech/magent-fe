6861ef7f4717240bfc95a0dc08bf7edaa5b6949c
Modupe Akanni <modupe775@gmail.com>
2025-06-21 22:14:53 +0100

    feat: add gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,12 +22,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG}`}
+        />
         <Script id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GTAG || ''});
+            gtag('config', '${process.env.NEXT_PUBLIC_GTAG}');
           `}
         </Script>
       </head>
