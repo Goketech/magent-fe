a434e3969bebe3010524d4e7af9f7cb791d67f5a
Dev_Faruq <olatoyesefaruq@gmail.com>
2025-06-21 07:37:40 +0100

    fix: redirect to dashbaord after sign up

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -267,7 +267,7 @@ function page() {
         description: "Registration successful!",
       });
 
-      navigate.push("/login");
+      navigate.push("/dashboard");
     } catch (error: any) {
       // ✅ Handle error
       const errorMessage =
